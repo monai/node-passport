d7850ecc4f13112945d738836a5db4762b83720d
monai <monai@cure.lt>
2019-10-11 16:21:55 +0300

    Implement SecureReader

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const selectApplication = require('./lib/doc9309/selectApplication');
 const { performBac } = require('./lib/doc9309/bac');
 const { dbak } = require('./lib/doc9309/dbak');
 const { readFileSF } = require('./lib/doc9309/readFile');
+const SecureReader = require('./lib/secureReader');
 
 const kmrz = process.env.KMRZ;
 if ( ! kmrz) {
@@ -19,8 +20,9 @@ async function work() {
 
     await selectApplication(reader);
     const session = await performBac(reader, dbak(kmrz));
+    const sreader = new SecureReader(reader, session);
 
-    const file = await readFileSF(reader, session, '011e');
+    const file = await readFileSF(sreader, '011e');
     console.log(file);
   } catch (ex) {
     console.error(ex);
