825112d7f3463413ea6a23c489cac65a9b88eedb
monai <monai@cure.lt>
2019-10-16 22:34:33 +0300

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const fs = require('fs');
 const Reader = require('./lib/reader');
 const selectApplication = require('./lib/doc9309/selectApplication');
 const { performBac } = require('./lib/doc9309/bac');
 const { dbak } = require('./lib/doc9309/dbak');
 const { readFileSelect, readFileSfi } = require('./lib/doc9309/readFile');
 const SecureReader = require('./lib/secureReader');
+const decodeFile = require('./lib/decodeFile');
 
 const kmrz = process.env.KMRZ;
 if ( ! kmrz) {
@@ -26,7 +28,29 @@ async function work() {
     // console.log(fileSelect);
 
     const fileSfi = await readFileSfi(sreader, 0x1e);
-    console.log(fileSfi);
+    decodeFile(fileSfi);
+
+    // const dg01 = await readFileSfi(sreader, 0x01);
+    // console.log(dg01);
+    // console.log(dg01.toString());
+
+    // const dg02 = await readFileSfi(sreader, 0x02);
+    // fs.writeFileSync('photo.jpg', dg02);
+    // console.log(dg02);
+    // console.log(dg02.toString());
+
+    // const dg03 = await readFileSfi(sreader, 0x03);
+    // console.log(dg03);
+    // console.log(dg03.toString());
+
+    // const dg11 = await readFileSfi(sreader, 0x0b);
+    // console.log(dg11);
+    // console.log(dg11.toString());
+
+    // const dg14 = await readFileSfi(sreader, 0x0e);
+    // console.log(dg14);
+    // console.log(dg14.toString());
+
   } catch (ex) {
     console.error(ex);
   }
