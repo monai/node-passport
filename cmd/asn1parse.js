8725e4adf6e71910f511f51d6f22c87cc2a6427b
monai <monai@cure.lt>
2021-05-16 12:44:44 +0300

    Move file

--- cmd/asn1parse.js
+++ cmd/asn1parse.js
@@ -4,7 +4,7 @@
 const { resolve } = require('path');
 const minimist = require('minimist');
 const stream = require('stream-util2');
-const parse = require('../lib/asn1/parse');
+const parse = require('../lib/asn1/util/parse');
 const inspect = require('../lib/asn1/tree_inspect');
 const { noTail } = require('../lib/util');
 
