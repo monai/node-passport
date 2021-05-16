a6620cca5299747ee41385e44fafda04d94c498d
monai <monai@cure.lt>
2021-05-16 13:22:31 +0300

    Streamify

--- cmd/asn1parse.js
+++ cmd/asn1parse.js
@@ -2,12 +2,15 @@
 /* eslint-disable global-require */
 /* eslint-disable no-console */
 const { resolve } = require('path');
+const { pipeline } = require('stream');
 const minimist = require('minimist');
 const stream = require('stream-util2');
-const parse = require('../lib/asn1/util/parse');
 const noTail = require('../lib/asn1/util/no_tail');
-const inspect = require('../lib/asn1/tree_inspect');
-const treeForcedParser = require('../lib/asn1/tree_forced_parser');
+
+const ParserStream = require('../lib/asn1/parser_stream');
+const TreeStream = require('../lib/asn1/tree_stream');
+const TreeInspectStream = require('../lib/asn1/tree_inspect_stream');
+const TreeForcedParserStream = require('../lib/asn1/tree_forced_parser_stream');
 
 module.exports = main;
 
@@ -23,33 +26,37 @@ function main(argv) {
     },
   );
 
-  process
-    .stdin
-    .pipe(stream.buffer())
-    .pipe(stream.writable((chunk, done) => {
-      if (argv.hex) {
-        chunk = Buffer.from(chunk.toString(), 'hex');
-      }
+  pipeline([
+    process.stdin,
+    argv.hex && transformify(toHex),
+    new ParserStream(),
+    new TreeStream(),
+    argv.force && new TreeForcedParserStream(),
+    argv.tail === false && transformify(noTail),
+    new TreeInspectStream({
+      colors: true,
+      type: type(argv.type),
+    }),
+    stream.consoleLog(),
+  ].filter(Boolean), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
-      let tree = parse(chunk);
-      if (argv.force) {
-        tree = treeForcedParser(tree);
-      }
+function toHex(buf) {
+  return Buffer.from(buf.toString(), 'hex');
+}
 
-      const options = {
-        depth: 20,
-        colors: true,
-        type: type(argv.type),
-      };
-
-      if (argv.tail === false) {
-        tree = noTail(tree);
-      }
-
-      console.log(inspect(tree, options));
-
-      done();
-    }));
+function transformify(fn) {
+  return stream.transform((chunk, done) => {
+    try {
+      done(null, fn(chunk));
+    } catch (ex) {
+      done(ex);
+    }
+  });
 }
 
 function type(arg) {
