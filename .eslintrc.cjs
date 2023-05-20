b4011c37be7781dca4d7daf8208300d80b297521
monai <monai@cure.lt>
2023-05-20 13:33:33 +0300

    Add typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,14 @@
 module.exports = {
+  root: true,
   extends: [
     '@monai/base',
     '@monai/style',
     '@monai/node',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-bitwise': 'off',
     'no-param-reassign': 'off',
@@ -13,5 +18,27 @@ module.exports = {
         devDependencies: ['**/__test__/**/*.mjs'],
       },
     ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        cjs: 'never',
+        ts: 'never',
+        cts: 'never',
+        mts: 'never',
+      },
+    ],
+    'node/no-missing-import': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.cts', '.mts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
